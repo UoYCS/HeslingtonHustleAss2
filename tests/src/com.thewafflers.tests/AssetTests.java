ed11bd098f475a2d15fb65f7d6ac317e1cd000fe
Merrill <mxd400@icloud.com>
2024-04-30 13:15:09 +0100

    update asset tests
    added comments indicating future asset tests to be made

--- tests/src/com.thewafflers.tests/AssetTests.java
+++ tests/src/com.thewafflers.tests/AssetTests.java
@@ -9,12 +9,27 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
 public class AssetTests {
+
+
+  /*
+  before these tests are completed the classes should be refactored so they
+  have string storing the name of the asset file, which are used in the code
+  instead of directly using filename when creating textures
+   */
   @Test
   public void testMenuScreenAssetExists() {
     assertTrue("The png asset for menu screen exists",
         Gdx.files.internal("MenuScreen.png").exists());
 
-    // this needs to be changed to a variable in MenuScreen which holds the png
-    // once that has been implemented in MenuScreen
   }
+
+  // tests for remaining screen assets
+
+  // tests for all the button assets
+
+  // tests for icon assets
+
+  // tests for sprite sheet assets
+
+  // tests for any other used assets
 }
