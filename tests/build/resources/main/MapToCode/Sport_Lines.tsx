12e833693fba79bac21322e6717d5135adcf1183
Merrill <mxd400@icloud.com>
2024-05-20 23:23:07 +0100

    build changes
    I think build might be needed to add CI, we will see...

---
+++ tests/build/resources/main/MapToCode/Sport_Lines.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Sport_Lines" tilewidth="16" tileheight="16" tilecount="3328" columns="52">
+ <image source="Sport_Lines.png" width="832" height="1024"/>
+</tileset>
