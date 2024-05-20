9c3f2afcfd17fee868afebd5910c3f5e8a03bf34
Merrill <mxd400@icloud.com>
2024-05-20 23:40:01 +0100

    We need build (i think)
    we need build for CI, i think, due to error 126 "No Gradle build results detected."

---
+++ desktop/build/docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.waddle_ware.heslington_hustle","c":"DesktopLauncher","l":"DesktopLauncher()","url":"%3Cinit%3E()"},{"p":"com.waddle_ware.heslington_hustle","c":"DesktopLauncher","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ desktop/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.waddle_ware.heslington_hustle","l":"DesktopLauncher"}]
